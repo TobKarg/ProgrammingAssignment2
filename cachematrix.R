d3368ceb7e1fefc6cd6c5298b9a4e8728f945466
TobKarg <tobias.kargus@outlook.de>
2021-08-24 18:15:10 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function to invert a matrix with using closures
+## X and inv_X are stored in enclosing environments
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(X = matrix()) {
+  X_inv <- NULL
+  set <- function(Y) {
+    X <<- Y
+    X_inv <<- NULL
+  }
+  # Defines the Matrix to be inverted in the environment
+  
+  get <- function() X
+  # Displays the matrix
+  setInverse <- function() X_inv <<- solve(X)
+  # Defines the inverting of the Matrix
+  getInverse <- function() X_inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Outputs the Inverse of a Matrix with checking before whether
+## inverse was stored before
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(X, ...) {
+  X_inv <- X$getInverse()
+  if(!is.null(X_inv)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- X$get()
+  X_inv <- solve(data, ...)
+  X$setInverse(m)
+  X
 }
